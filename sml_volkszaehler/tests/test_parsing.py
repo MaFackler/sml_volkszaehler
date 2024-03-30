6191de56666364863bddaae219b5cf38e6e9e2a1
MaFackler <17232532+MaFackler@users.noreply.github.com>
2024-03-30 11:23:01 +0100

    Refactor and test data

--- sml_volkszaehler/tests/test_parsing.py
+++ sml_volkszaehler/tests/test_parsing.py
@@ -1,7 +1,16 @@
 import io
 import pytest
 from binascii import hexlify
-data = "TODODATA"
+data = """
+1b 1b 1b 1b
+01 01 01 01
+72
+  01
+  01
+81 01 ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+1b 1b 1b 1b
+1a 00 00 00
+"""
 
 from sml_volkszaehler import get_values_from_stream, MeterValue
 
@@ -9,28 +18,34 @@ from sml_volkszaehler import get_values_from_stream, MeterValue
 @pytest.fixture
 def socket():
     res = bytearray()
+    global data
+    data = data.replace("\n", " ")
     for byte in data.split(" "):
-        res += bytearray.fromhex(byte)
+        if byte:
+            res += bytearray.fromhex(byte)
     yield io.BytesIO(bytes(res))
 
 
 def test_parsing(socket):
     res = get_values_from_stream(socket)
-    assert res["1.8.0"] == MeterValue(value=180, scaler=0.1, unit='Wh')
-    assert res["1.8.1"] == MeterValue(value=181, scaler=0.1, unit='Wh')
-    assert res["1.8.2"] == MeterValue(value=182, scaler=0.1, unit='Wh')
+    # print(res)
+    assert res[0] == [b"\x01", b"\x01"]
+    assert len(res[1]) == 17
+    # assert res["1.8.0"] == MeterValue(value=180, scaler=0.1, unit='Wh')
+    # assert res["1.8.1"] == MeterValue(value=181, scaler=0.1, unit='Wh')
+    # assert res["1.8.2"] == MeterValue(value=182, scaler=0.1, unit='Wh')
 
-    assert res["2.8.0"] == MeterValue(value=280, scaler=0.1, unit='Wh')
-    assert res["2.8.1"] == MeterValue(value=281, scaler=0.1, unit='Wh')
-    assert res["2.8.2"] == MeterValue(value=282, scaler=0.1, unit='Wh')
+    # assert res["2.8.0"] == MeterValue(value=280, scaler=0.1, unit='Wh')
+    # assert res["2.8.1"] == MeterValue(value=281, scaler=0.1, unit='Wh')
+    # assert res["2.8.2"] == MeterValue(value=282, scaler=0.1, unit='Wh')
 
-    assert res["16.7.0"] == MeterValue(value=1670, scaler=1, unit='W')
-    assert res["36.7.0"] == MeterValue(value=3670, scaler=1, unit='W')
-    assert res["56.7.0"] == MeterValue(value=5670, scaler=1, unit='W')
-    assert res["76.7.0"] == MeterValue(value=7670, scaler=1, unit='W')
+    # assert res["16.7.0"] == MeterValue(value=1670, scaler=1, unit='W')
+    # assert res["36.7.0"] == MeterValue(value=3670, scaler=1, unit='W')
+    # assert res["56.7.0"] == MeterValue(value=5670, scaler=1, unit='W')
+    # assert res["76.7.0"] == MeterValue(value=7670, scaler=1, unit='W')
 
-    assert res["199.130.3"] == MeterValue(value=b"FOO", scaler=1, unit=None)
-    assert res["199.130.5"] == MeterValue(value=b"BAR", scaler=1, unit=None)
+    # assert res["199.130.3"] == MeterValue(value=b"FOO", scaler=1, unit=None)
+    # assert res["199.130.5"] == MeterValue(value=b"BAR", scaler=1, unit=None)
 
 
 
--- sml_volkszaehler/volkszaehler.py
+++ sml_volkszaehler/volkszaehler.py
@@ -33,24 +33,68 @@ def _read_value_list_from_stream(stream, length, level=0):
     return res
 
 
-def _split_byte(b: bytes):
-    assert len(b) == 1
-    first = int.from_bytes(b) >> 4
-    second = int.from_bytes(b) & 0b00001111
-    return (first, second)
+def _split_byte(message: bytearray):
+    b = int.to_bytes(message[0], 1)
+    as_int: int = int.from_bytes(b)
+    first = as_int >> 4
+    read_next_byte = first >> 3
+    second = as_int & 0b00001111
+    n = 1
+    if read_next_byte:
+        b2 = message[1]
+        second = (second << 4) + message[1]
+        n += 1
+
+    return (first, second, n)
 
 
 def _read_msg_start(stream):
-    message = []
-    while message[-4:] != [b'\x1b', ] * 4:
-        message.append(stream.read(1))
-    _ = stream.read(4)  # Version
+    message = bytearray()
+    while message[-4:] != bytearray(b'\x1b\x1b\x1b\x1b'):
+        message += bytearray(stream.read(1))
+    version = stream.read(4)  # Version
+    assert version == b"\x01\x01\x01\x01"
+    message += bytearray(version)
+    return message
+
+def _read_until_end(stream, message):
+    while message[-5:] != bytearray(b"\x1b\x1b\x1b\x1b\x1a"):
+        message += bytearray(stream.read(1))
+    message += bytearray(stream.read(3))
+
+
+def _parse_list(message, res):
+    a, b, n = _split_byte(message)
+    if a == 7:
+        message = message[n:]
+        children = []
+        for i in range(0, b):
+            message = _parse_list(message, children)
+        res.append(children)
+        return message
+    else:
+        b = max(b, 1)  # NOTE: 0x00 handling
+        assert len(message) >= b, f"{len(message)} vs. {b}"
+        res.append(message[0:b])
+        return message[b:]
+        
+
 
 
 def get_values_from_stream(stream):
-    _read_msg_start(stream)
+    message = _read_msg_start(stream)
+    _read_until_end(stream, message)
+
+    payload = message[8:-8]
+    res = []
+    while len(payload) > 0:
+        payload = _parse_list(payload, res)
+        print("rest payload", payload)
+
+    return res
     values = []
     identifier, length = _split_byte(stream.read(1))
+    print(identifier, length)
     while identifier == 7:
         values.append(_read_value_list_from_stream(stream, length, level=1))
         identifier, length = _split_byte(stream.read(1))
